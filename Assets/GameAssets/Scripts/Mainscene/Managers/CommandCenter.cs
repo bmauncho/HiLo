42f423af9110576a6d3bee7e7e89d8967edd2a3e
BradleyMauncho <bmauncho@gmail.com>
2025-07-03 15:42:33 +0300

    SetUP_2.0

---
+++ Assets/GameAssets/Scripts/Mainscene/Managers/CommandCenter.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+[System.Serializable]
+public enum GameMode
+{
+    Demo,
+    Live,
+}
+public class CommandCenter : MonoBehaviour
+{
+    public static CommandCenter Instance { get; private set; }
+    public GameMode gameMode; // Live or Demo
+    public PoolManager poolManager_;
+    public SettingsManager settingsManager_;
+    public SoundManager soundManager_;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
