a918ba446ad2de074794201a5ccef33cee89e528
BradleyMauncho <bmauncho@gmail.com>
2025-07-11 16:14:26 +0300

    update

---
+++ Assets/GameAssets/Scripts/Mainscene/Managers/MultiplierManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+public enum MultiplierType
+{
+    None = 0,
+    High,
+    Same,
+    Low,
+}
+
+public class MultiplierManager : MonoBehaviour
+{
+    public MultiplierType selectedMultiplier;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
